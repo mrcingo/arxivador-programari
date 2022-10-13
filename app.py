cd10c427112b9196f91728baabb41013e9242301
mrcingo <mrcingosup@gmail.com>
2022-10-13 16:54:57 +0200

    ordenant el projecte

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
 import flask
 
 app = flask.Flask(__name__)
+                     
+# modelos de prueba
 
 @app.route('/test/index')
-def index():
+def test_index():
     return flask.render_template('tests/index.html')
 
 @app.route('/test/login')
-def index():
+def test_login():
     return flask.render_template('tests/login.html')
 
 @app.route('/test/register')
+def test_register():
+    return flask.render_template('tests.register.html')
+
+# modelos de produccion
+
+@app.route('/index')
 def index():
-    return flask.render_template('tests/register.html')
+    return flask.render_template('production/index.html')
+
+@app.route('/login')
+def login():
+    return flask.render_template('production/login.html')
+
+@app.route('/register')
+def register():
+    return flask.render_template('production/register.html')
 
 if __name__ == '__main__':
     app.run()
