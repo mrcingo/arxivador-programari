7bb2f524a43456892806933fbed1b7c4873e4d38
mrcingo <115689923+mrcingo@users.noreply.github.com>
2022-10-14 11:46:18 +0200

    register test page

--- app.py
+++ app.py
@@ -14,11 +14,19 @@ def test_login():
     with open('users.json', 'r') as fr:
         users: dict = json.load(fr)
     if users.get(flask.request.args.get('username')):
-        return flask.redirect('/tests/index')
+        return flask.redirect('/test/index')
     return flask.render_template('tests/login.html')
 
 @app.route('/test/register')
 def test_register():
+    with open('users.json', 'r') as fr:
+        users: dict = json.load(fr)
+
+    if not users.get(flask.request.args.get('username')) and flask.request.args.get('username') and flask.request.args.get('password'):
+        with open('users.json', 'w') as fw:
+            users[flask.request.args.get('username')] = users[flask.request.args.get('password')] 
+            json.dump(users, fw)
+        return flask.redirect('/test/index')
     return flask.render_template('tests/register.html')
 
 # modelos de produccion
