80fe348be421b5c989d45ed66bca02ba9c948ca8
mrcingo <115689923+mrcingo@users.noreply.github.com>
2022-10-25 08:29:49 +0200

    dasdasd

--- manage.py
+++ manage.py
@@ -7,13 +7,15 @@ class Manage(Connection):
 
     def __init__(self, database: str) -> None:
         super().__init__(database)
-        self.execute('CREATE TABLE IF NOT EXISTS clients (id INT, username CHAR(255), password CHAR(255), session CHAR(255));')
+        self.execute(
+            'CREATE TABLE IF NOT EXISTS clients (id INT, username CHAR(255), password CHAR(255), session CHAR(255));')
 
     def create(self, username: str, password: str) -> dict[str, str] | None:
         if not username and not password:
             return None
 
-        if not self.execute(f'SELECT * FROM clients WHERE username=\'{username}\'').fetchone() is None:
+        if not self.execute(
+            f'SELECT * FROM clients WHERE username=\'{username}\'').fetchone() is None:
             return None
             
         id = self.execute(f'SELECT MAX(id) FROM clients').fetchone()
@@ -25,6 +27,8 @@ class Manage(Connection):
         return {'username': username, 'password': password, 'sid': sid}
 
     def session(self, sid: str) -> bool:
+        print(self.execute(
+            f'SELECT * FROM clients WHERE session=\"{sid}\"').fetchone())
         return True if self.execute(
             f'SELECT * FROM clients WHERE session=\"{sid}\"').fetchone() != None else False
 
