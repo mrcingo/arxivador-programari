9775ae4166ee06f19a72233589779a4c51f4e381
mrcingo <115689923+mrcingo@users.noreply.github.com>
2022-10-27 09:51:00 +0200

    caca

---
+++ account.py
@@ -0,0 +1,66 @@
+from sqlite3 import Connection
+
+from string import ascii_letters, digits
+from random import choices
+
+
+class Account(Connection):
+
+    def __init__(self, database: str) -> None:
+        super().__init__(database)
+        self.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS accounts 
+            (
+                id INT, 
+                username CHAR(255), 
+                password CHAR(255), 
+                session CHAR(255)
+            );
+            ''')
+
+    def register(self, username: str, password: str) -> dict | None:
+        account = self.execute(f'SELECT * FROM accounts WHERE username="{username}"').fetchone()
+
+        if not username or not password:
+            return None
+
+        if account:
+            return None
+
+        indentificator = self.execute(f'SELECT MAX(id) FROM accounts').fetchone()
+        if not indentificator:
+            indentificator = 0
+        indentificator += 1
+
+        session = choices(
+            ascii_letters + digits,
+            k = 32
+        )
+
+        self.execute(f'''
+        INSERT INTO accounts 
+        (id, username, password, session) VALUES 
+        ({indentificator}, "{username}", "{password}", "{session}")
+        ''')
+        self.commit()
+
+        return {
+            'id': indentificator, 
+            'username': username, 
+            'password': password,
+            'session': session
+        }
+
+    def login(self, username: str, password: str):
+        account = self.execute(f'SELECT * FROM accounts WHERE username="{username}"').fetchone()
+        
+        if not account:
+            return None
+
+        return {
+            'id': account[0],
+            'username': account[1],
+            'password': account[2],
+            'session': account[3]
+        }
--- app.py
+++ app.py
@@ -1,62 +1,16 @@
 import flask
-from manage import Manage
 
+from .account import Account
 
 app = flask.Flask(__name__)
-manager = Manage('sqlite3.db')
+accmanager = Account('sqlite3.db')
 
-@app.route('/')
-def index():
-    token = manager.session(flask.request.args.get('SID'))
-    if token:
-        return flask.jsonify(token)
-    return flask.render_template('index.html', cookie=0)
-
-@app.route('/products')
-def products():
-    print(flask.request.args.get('SID'))
-    token = manager.session(flask.request.args.get('SID'))
-
-    if token:
-        return flask.render_template('products.html')
-    return flask.redirect('/')
-
-@app.route('/logout')
-def logout():
-    if flask.request.cookies.get('SID'):
-        response = flask.make_response(flask.redirect('/login'))
-        response.set_cookie('SID', '', expires=0)
-        return response
-    return '<h1>Not loged in.</h1>'
-
-@app.route('/login', methods = ['GET'])
+@app.route('/login')
 def login():
-    username = flask.request.args.get('username')
-    password = flask.request.args.get('password')
-    
-    client = manager.exist(username, password)
-    if client:
-        response = flask.make_response(
-            flask.redirect('/'))
-        response.set_cookie('SID', client[-1])
-        return response
-    return flask.render_template('login.html')
+    username = flask.request.form.get('username')
+    password = flask.request.form.get('password')
 
-@app.route('/register', methods = ['GET', 'POST'])
-def register():
-    username = flask.request.args.get('username')
-    password = flask.request.args.get('password')
-    
-    client = manager.create(username, password)
-    if client:
-        response = flask.make_response(flask.redirect('/'))
-        response.set_cookie('SID', client['sid'])
-        return response
-    
-    if username:
-        return flask.render_template('register.html', username = username)
-    else:
-        return flask.render_template('register.html')
+    return accmanager.login(username, password)
     
 
 if __name__ == "__main__":
