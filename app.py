bc12d4ab24d906b4dcff1dbff44d4d1135ac43de
mrcingo <115689923+mrcingo@users.noreply.github.com>
2022-10-27 11:03:49 +0200

    dasdasdasdasdasdada

--- app.py
+++ app.py
@@ -5,6 +5,14 @@ from account import Account
 app = flask.Flask(__name__)
 accmanager = Account('sqlite3.db')
 
+@app.route('/index')
+def index():
+    session = flask.request.cookies.get('SID')
+    account_session = accmanager.session(session)
+    if not account_session:
+        return flask.render_template('index.html', cookie = 0)
+    return flask.render_template('index.html', cookie = 1)
+
 @app.route('/login')
 def login():
     username = flask.request.args.get('username')
@@ -27,10 +35,9 @@ def register():
     password = flask.request.args.get('password')
 
     account = accmanager.register(username, password)
-    print(account)
     if account:
         response = flask.make_response(
-            flask.redirect('index.html')
+            flask.redirect('/index')
         )
         response.set_cookie('SID', account['session'])
 
